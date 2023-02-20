c9228f70ba8bb3bf54fc02870f21d5c3065231af
Ahmed Zougari <zougariahmed007@gmail.com>
2023-02-20 10:42:03 +0000

    fix infinite redirect

--- src/components/advice-generator-app/Advice.tsx
+++ src/components/advice-generator-app/Advice.tsx
@@ -3,7 +3,6 @@ import useAdvice from '../../hooks/advice-generator-app/useAdvice'
 export default function Advice() {
   const { advice, loading, error, getNewAdvice } = useAdvice()
 
-  console.log(!!(!loading && !error))
   return (
     <main className='advice-shadow absolute top-1/2 left-1/2 w-[min(90%,540px)] -translate-y-1/2 -translate-x-1/2 rounded-2xl bg-[hsl(var(--dark-grayish-blue))] px-[19.67px] pt-[39px] pb-[66.48px] text-center font-["Manrope"] lg:px-[48px] lg:pb-[79px] lg:pt-[47px]'>
       {/* error */}
